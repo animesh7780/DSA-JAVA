9c02461958d77a0461f57c98321f12dac8260f72
animesh7780 <thakuranimesh235@gmail.com>
2025-01-31 21:34:04 +0530

    Update Leet48.java

--- Questions/Leetcode/Leet48.java
+++ Questions/Leetcode/Leet48.java
@@ -19,12 +19,11 @@ public class Leet48 {
                 matrix[col][row] = temp;
             }
         }
-
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < matrix[0].length / 2; col++) {
-                int temp = matrix[row][col];
-                matrix[row][col] = matrix[row][matrix[0].length - 1 - col];
-                matrix[row][matrix[0].length - 1 - col] = temp;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[0].length - 1 - j];
+                matrix[i][matrix[0].length - 1 - j] = temp;
             }
         }
     }
