6bdf309168ab39b85ad2c537790a5d1fa58872df
Animesh Kumar <143063970+animesh7780@users.noreply.github.com>
2025-06-18 23:41:55 +0530

    Stack

---
+++ StackAndQueues/Stack/NextGreaterElement.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class NextGreaterElement {
+public static int[] nextSmallerElements(int[] nums) {
+    int n = nums.length;
+    int[] res = new int[n];
+    Stack<Integer> st = new Stack<>();
+    
+    for (int i = 2 * n - 1; i >= 0; i--) {
+        while (!st.isEmpty() && st.peek() <= nums[i % n]) st.pop();
+        if (i < n) res[i] = st.isEmpty() ? -1 : st.peek();
+        st.push(nums[i % n]);
+    }
+    return res;
+}
+}
