a62a990d2c46a10a22d87fb5ffaf82cb250d64c1
Animesh Kumar <143063970+animesh7780@users.noreply.github.com>
2025-06-30 23:27:07 +0530

    Linked Lists

--- Linked Lists/Questions/Sort0sand1s.java
+++ Linked Lists/Questions/Sort0sand1s.java
@@ -2,27 +2,78 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Sort0sand1s {
+
+    // public ListNode sortLL(ListNode head){
+    //     if(head == null) return null;
+
+    //     //convert to list
+    //     ArrayList<Integer> list = new ArrayList<>();
+    //     ListNode current  = head;
+    //     while(current != null){
+    //         list.add(current.val);
+    //         current = current.next;
+    //     }
+
+    //     Collections.sort(list);
+
+    //     ListNode newHead = new ListNode(list.get(0));
+    //     current = newHead;
+    //     for(int i=1; i<list.size(); i++){
+    //         current.next = new ListNode(list.get(i));
+    //         current = current.next;
+    //     }
+
+    //     return newHead;
+    // }
+
     public ListNode sortLL(ListNode head){
         if(head == null) return null;
 
-        //convert to list
-        ArrayList<Integer> list = new ArrayList<>();
-        ListNode current  = head;
-        while(current != null){
-            list.add(current.val);
-            current = current.next;
+        ListNode mid = getMid(head);
+
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null; //break the link between the right and left
+        
+        //breaking them to sort them out
+        left = sortLL(left);
+        right = sortLL(right);
+
+        return mergeLL(left, right);
+    }
+
+    public ListNode getMid(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        Collections.sort(list);
+        return slow;
+    }
 
-        ListNode newHead = new ListNode(list.get(0));
-        current = newHead;
-        for(int i=1; i<list.size(); i++){
-            current.next = new ListNode(list.get(i));
-            current = current.next;
+    public ListNode mergeLL(ListNode left, ListNode right){
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        //sorting them out
+        while(left != null && right != null){
+            if(left.val < right.val){
+                curr.next = left;
+                left = left.next;
+            }else{
+                curr.next = right;
+                right = right.next;
+            }
+            curr = curr.next;
         }
 
-        return newHead;
+        curr.next = (left != null) ? left : right;
+
+        return dummy.next;
+
     }
 }
 
