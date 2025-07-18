2e7f3f393edf756b398b30edc023dceac20d4605
animesh7780 <thakuranimesh235@gmail.com>
2025-07-18 22:54:33 +0530

    Sliding Winodw

--- 2-Pointer OR Sliding Window/MinWindowSubString.java
+++ 2-Pointer OR Sliding Window/MinWindowSubString.java
@@ -1,16 +1,50 @@
 public class MinWindowSubString {
     public String minWindow(String s, String t) {
-        int left = 0, minLen = Integer.MAX_VALUE;
-        String res = "";
-        int n = s.length();
+        if(s == null || t == null || s.length() < t.length()){
+            return "";
+        }
 
-        for(int right=0; right<n; right++){
-            int len = 0;
-            if(s.contains(t)){
-                len = right - left + 1;
-                minLen = Math.min(minLen, len);
-                res += right;
+        int[] tcount = new int[128];
+        for(char c : t.toCharArray()){
+            tcount[c]++;
+        }
+
+        int left = 0, minLen = Integer.MAX_VALUE;
+        int minStart = 0; //starting index of window
+        int req = t.length();
+
+        for(int right=0; right<s.length(); right++){
+            char rightChar = s.charAt(right);
+
+            //if character is needed, decrement req count
+            if(tcount[rightChar] > 0){
+                req--;
+            }
+
+            tcount[rightChar]--;//Always decrement count window
+
+            // When we have a valid window (all characters matched)
+            while (req == 0) {
+                // Update minimum window if current is smaller
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    minStart = left;
+                }
+                
+                // Try to shrink window from left
+                char leftChar = s.charAt(left);
+                tcount[leftChar]++; // Add back the character we're removing
+                
+                // If removing this character breaks our window
+                if (tcount[leftChar] > 0) {
+                    req++;
+                }
+                
+                left++;
             }
         }
+        
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
     }
 }
+
