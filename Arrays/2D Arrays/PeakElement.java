a1a1eab9dcd60e751791799c3b68f05770661f6a
animesh7780 <thakuranimesh235@gmail.com>
2025-07-20 21:37:28 +0530

    Arrays

--- Arrays/2D Arrays/PeakElement.java
+++ Arrays/2D Arrays/PeakElement.java
@@ -1,5 +1,33 @@
 public class PeakElement {
     public int[] findPeakGrid(int[][] mat) {
-        
+        int low = 0;
+        int high = mat[0].length;
+
+        while(low <= high){
+            int max = Integer.MIN_VALUE;
+            int mid = low + (high - low)/2;
+            int maxRow = 0;
+
+            for(int i=0; i<mat.length;i++){
+                if(mat[i][mid] > max){
+                    max = mat[i][mid];
+                    maxRow = i;
+                } 
+            }
+            
+            boolean isLeftSmaller = (mid == 0 || mat[maxRow][mid-1] <= mat[maxRow][mid]);
+            boolean isRightSmaller = (mid == mat[0].length || mat[maxRow][mid+1] <= mat[maxRow][mid]);
+
+            if(isLeftSmaller && isRightSmaller){
+                return new int[] {maxRow ,mid};
+            }
+            else if(mid > 0 && mat[maxRow][mid-1] > mat[maxRow][mid]){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return new int[]{-1, -1};
     }
 }
