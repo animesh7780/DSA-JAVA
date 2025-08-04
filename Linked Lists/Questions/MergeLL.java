a561da52547ae4f539a16323b2c4d945a00dfc22
animesh7780 <thakuranimesh235@gmail.com>
2025-08-04 23:09:47 +0530

    LL

---
+++ Linked Lists/Questions/MergeLL.java
@@ -0,0 +1,38 @@
+public class MergeLL {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // Dummy node to start the merged list
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        // Merge while both lists are not null
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        // Attach the remaining nodes, if any
+        if (list1 != null) {
+            current.next = list1;
+        } else if (list2 != null) {
+            current.next = list2;
+        }
+
+        return dummy.next; // Return the head of the merged list
+    }
+
+}
+
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
