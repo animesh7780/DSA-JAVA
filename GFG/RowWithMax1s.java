7f57fabde306201317c4f0d378bc1c06863fc98a
animesh7780 <thakuranimesh2352gmail.com>
2025-12-22 23:08:51 +0530

    POTD-GFG

---
+++ GFG/RowWithMax1s.java
@@ -0,0 +1,18 @@
+public class RowWithMax1s {
+    public int rowWithMax1s(int arr[][]) {
+        // code here
+        int n = arr.length;
+        int j = arr[0].length-1;
+        int ans = -1;
+        for(int i=0; i<n; i++){
+            while(j>=0 && arr[i][j]==1){
+               ans = i; 
+               j--;
+            }
+        
+        if(j==-1) return i;
+        }
+        return ans;
+    }
+    
+}
